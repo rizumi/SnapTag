ce35f76818bb6631ed39e589e114fd0a36044f93
izumi <rizumi222@gmail.com>
2024-11-26 01:01:16 +0900

    Add TagRepository

---
+++ SnapTag/Repository/TagRepository.swift
@@ -0,0 +1,26 @@
+//
+//  TagRepository.swift
+//  SnapTag
+//
+//  Created by izumi on 2024/11/26.
+//
+
+import SwiftData
+
+protocol TagRepositoryProtocol {
+    func fetch() -> [Tag]
+}
+
+final class TagRepository: TagRepositoryProtocol {
+    private let context: ModelContext
+
+    init(
+        context: ModelContext
+    ) {
+        self.context = context
+    }
+
+    func fetch() -> [Tag] {
+        return (try? context.fetch(FetchDescriptor<Tag>())) ?? []
+    }
+}
