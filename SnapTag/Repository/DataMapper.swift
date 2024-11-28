7e4ffd45b8061475769a8229f40406309bc905a7
izumi <rizumi222@gmail.com>
2024-11-28 23:52:29 +0900

    update project file

---
+++ SnapTag/Repository/DataMapper.swift
@@ -0,0 +1,18 @@
+//
+//  DataMapper.swift
+//  SnapTag
+//
+//  Created by izumi on 2024/11/28.
+//
+
+extension TagModel {
+    func toTag() -> Tag {
+        return .init(id: id, name: name)
+    }
+}
+
+extension SnapModel {
+    func toSnap() -> Snap {
+        return .init(id: id, imagePath: imagePath, tags: tags.map { $0.toTag() })
+    }
+}
