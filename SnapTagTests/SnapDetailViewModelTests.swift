c52e17f09f0ac14426273476459ecf3f085d55aa
izumi <rizumi222@gmail.com>
2024-11-30 14:29:05 +0900

    update tests

--- SnapTagTests/SnapDetailViewModelTests.swift
+++ SnapTagTests/SnapDetailViewModelTests.swift
@@ -12,12 +12,16 @@ import Testing
 @MainActor
 struct SnapDetailViewModelTests {
 
-    @Test("指定したSnapのindexがcurrentIndexPathに設定されること")
+    @Test("指定したSnapのindexとtagが設定されること")
     func testCurrentIndexPath() async throws {
         // Arrange
-        let snapA = Snap(id: "A", imagePath: "", tags: [])
-        let snapB = Snap(id: "B", imagePath: "", tags: [])
-        let snapC = Snap(id: "C", imagePath: "", tags: [])
+        let tagA = Tag(id: "1", name: "a")
+        let tagB = Tag(id: "2", name: "b")
+        let tagC = Tag(id: "2", name: "c")
+
+        let snapA = Snap(id: "A", imagePath: "", tags: [tagA])
+        let snapB = Snap(id: "B", imagePath: "", tags: [tagA, tagB])
+        let snapC = Snap(id: "C", imagePath: "", tags: [tagC])
         let snaps = [snapA, snapB, snapC]
 
         // Act
@@ -25,6 +29,42 @@ struct SnapDetailViewModelTests {
 
         // Assert
         #expect(viewModel.currentIndexPath == .init(item: 1, section: 0))
+        #expect(viewModel.tags == [tagA.name, tagB.name])
     }
 
+    @Test("指定したindexのSnapのindexとtagが設定されること")
+    func testOnChangeSnap() async throws {
+        // Arrange
+        let tagA = Tag(id: "1", name: "a")
+        let tagB = Tag(id: "2", name: "b")
+        let tagC = Tag(id: "2", name: "c")
+
+        let snapA = Snap(id: "A", imagePath: "", tags: [tagA])
+        let snapB = Snap(id: "B", imagePath: "", tags: [tagA, tagB])
+        let snapC = Snap(id: "C", imagePath: "", tags: [tagC])
+        let snaps = [snapA, snapB, snapC]
+        let viewModel = SnapDetailViewModel(snap: snapB, snaps: snaps)
+
+        // Act
+        viewModel.onChangeSnap(0)
+
+        // Assert
+        #expect(viewModel.currentIndexPath == .init(item: 0, section: 0))
+        #expect(viewModel.tags == [tagA.name])
+    }
+
+    @Test("showUIの状態がtoggleすること")
+    func testOnTapView() async throws {
+        // Arrange
+        let snapA = Snap(id: "A", imagePath: "", tags: [])
+        let snaps = [snapA]
+        let viewModel = SnapDetailViewModel(snap: snapA, snaps: snaps)
+
+        // Act + Assert
+        viewModel.onTapView()
+        #expect(viewModel.showUI == false)
+
+        viewModel.onTapView()
+        #expect(viewModel.showUI == true)
+    }
 }
