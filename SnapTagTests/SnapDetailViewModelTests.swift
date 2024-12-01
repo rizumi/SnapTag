7369ae3a9e49d85372538a6dd37bb7955b4e646b
izumi <rizumi222@gmail.com>
2024-12-01 15:05:47 +0900

    fix tests

--- SnapTagTests/SnapDetailViewModelTests.swift
+++ SnapTagTests/SnapDetailViewModelTests.swift
@@ -15,6 +15,7 @@ struct SnapDetailViewModelTests {
     @Test("指定したSnapのindexとtagが設定されること")
     func testCurrentIndexPath() async throws {
         // Arrange
+        let repository = SnapRepositoryProtocolMock()
         let flow = SnapDetailViewFlowMock()
 
         let tagA = Tag(id: "1", name: "a")
@@ -27,7 +28,9 @@ struct SnapDetailViewModelTests {
         let snaps = [snapA, snapB, snapC]
 
         // Act
-        let viewModel = SnapDetailViewModel(snap: snapB, snaps: snaps, flow: flow)
+        let viewModel = SnapDetailViewModel(
+            snap: snapB, snaps: snaps,
+            repository: repository, flow: flow)
 
         // Assert
         #expect(viewModel.currentIndexPath == .init(item: 1, section: 0))
@@ -37,6 +40,7 @@ struct SnapDetailViewModelTests {
     @Test("指定したindexのSnapのindexとtagが設定されること")
     func testOnChangeSnap() async throws {
         // Arrange
+        let repository = SnapRepositoryProtocolMock()
         let flow = SnapDetailViewFlowMock()
 
         let tagA = Tag(id: "1", name: "a")
@@ -47,7 +51,9 @@ struct SnapDetailViewModelTests {
         let snapB = Snap(id: "B", imagePath: "", tags: [tagA, tagB])
         let snapC = Snap(id: "C", imagePath: "", tags: [tagC])
         let snaps = [snapA, snapB, snapC]
-        let viewModel = SnapDetailViewModel(snap: snapB, snaps: snaps, flow: flow)
+        let viewModel = SnapDetailViewModel(
+            snap: snapB, snaps: snaps,
+            repository: repository, flow: flow)
 
         // Act
         viewModel.onChangeSnap(0)
@@ -60,11 +66,14 @@ struct SnapDetailViewModelTests {
     @Test("showUIの状態がtoggleすること")
     func testOnTapView() async throws {
         // Arrange
+        let repository = SnapRepositoryProtocolMock()
         let flow = SnapDetailViewFlowMock()
 
         let snapA = Snap(id: "A", imagePath: "", tags: [])
         let snaps = [snapA]
-        let viewModel = SnapDetailViewModel(snap: snapA, snaps: snaps, flow: flow)
+        let viewModel = SnapDetailViewModel(
+            snap: snapA, snaps: snaps,
+            repository: repository, flow: flow)
 
         // Act + Assert
         viewModel.onTapView()
